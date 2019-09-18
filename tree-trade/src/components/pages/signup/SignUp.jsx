c09870ac0db64dfd75f681be07a5813d70daf951
Phi Trang <p.trang127@gmail.com>
2019-09-18 00:08:13 -0600

    css changes to sign up

--- tree-trade/src/components/pages/signup/SignUp.jsx
+++ tree-trade/src/components/pages/signup/SignUp.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SignUp.css';
 
-import {Form, Button } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
-const SignUp: React.FC = () => {
+class SignUp extends Component {
+  render() {
     return (
-        <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
+      <div class="signup signup-header">
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
     </Form.Text>
-  </Form.Group>
+        </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="I agree to the Terms & Conditions" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="I agree to the Terms & Conditions" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
   </Button>
-</Form>
+      </Form>
+      </div>
     );
+  }
 }
 
 export default SignUp;
